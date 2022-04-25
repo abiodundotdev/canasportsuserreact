e360913ead798e0150485e1f3b23e3679a4861b6
Canasports <104179199+Canasports@users.noreply.github.com>
2022-04-25 16:03:25 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let Api = axios.create({
-    baseURL : "http://localhost:8000/api/",
+    baseURL : "https://api.canasportsng.com/",
 });
 Api.defaults.withCredentials = true;
 export default Api;
